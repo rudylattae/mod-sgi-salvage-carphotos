8ff9b095c71314ad5c899a6d5e0dc588a23413e2
Rudy Lattae <rudylattae@gmail.com>
2014-01-03 01:17:07 -0600

    Partial improvement to item details manager

--- sandbox/app.js
+++ sandbox/app.js
@@ -3,9 +3,15 @@ var express = require('express'),
     app = express();
 
 
-app.configure(function() {
-    app.use(express.static(__dirname + '/public'));
-    app.use(harp.mount( __dirname + '/public'));
+// simple logger
+app.use(express.logger());
+
+app.use(express.static(__dirname + '/public'));
+app.use(harp.mount( __dirname + '/public'));
+
+app.get('/lcgi/salvage_bid_site/comp_details.cgi', function( req, res ) {
+    console.log( req.query );
+    res.send('Item details here.');
 });
 
 app.listen( 3000 );
