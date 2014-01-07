a51991f83f67a1cc905a6d393add9b67f41cce3f
Rudy Lattae <rudylattae@gmail.com>
2014-01-07 01:47:15 -0600

    Car photos -- first(ish) crack

---
+++ src/carPhotos.js
@@ -0,0 +1,87 @@
+var carPhotos = (function( $ ) {
+    "use strict";
+
+
+    // flag if global initialization has been completed
+    var ready = false;
+
+
+    function ItemsTableModManager( el ) {
+        var self = this;
+
+        self.init = function init() {
+            // setup table for mod
+            if ( el.attr('data-mod-ready') ) return;
+
+            el.find('thead tr').prepend('<th class="header">&nbsp;</th>');
+            el.find('tbody tr').each(function(i, row) {
+                $(row).prepend('<td class="js-mods"></td>'); 
+            });
+
+            el.attr('data-mod-ready', true);
+        };
+
+        self.install = function install( mod ) {
+            el.find('tbody tr').each(function(i, row) {
+                mod.install( row );
+            });
+        };
+
+        self.findStockNumberColumn = function findStockNumberColumn() {
+            var found = 0;
+
+            el.find('thead tr th').each(function(i, cell) {
+                if ( cell.innerText == "Stock Number" ) {
+                    found = i;
+                    return false;
+                }
+            });
+            return found;
+        };
+    }
+
+
+    function ItemThumbnailMod( stockNumberColumn ) {
+        var self = this,
+            mainPhotoUrlTemplate = '/images/salvage_images/{stockNumber}/main/1.jpg',
+            itemPhotoTemplate = 
+                '<div class="mod--thumbnail" target="_blank"> \
+                    <img alt="loading..." src="{src}" width="245"/> \
+                </div>';
+
+        self.install = function install( row ) {
+            if ( $('.js-mods .mod--thumbnail', row).length > 0 ) return;
+
+            var stockNumber = $('td:eq(' + stockNumberColumn + ')', row).text(),
+                thumbUrl = mainPhotoUrlTemplate.replace( '{stockNumber}', stockNumber ),
+                component = $.render( itemPhotoTemplate, { src: thumbUrl, stockNumber: stockNumber } );
+
+            $('.js-mods', row).append( component ); 
+        };
+
+        self.uninstall = function uninstall() {
+            throw new Error('Not implemented');
+        };
+    }
+
+
+    // perform global initialization
+    function init() {
+        if ( ready ) return;
+
+        var itemsTableElement = $('#bid_items').length > 0 ? $('#bid_items') : $('#bid_results'),
+            manager = new ItemsTableModManager( itemsTableElement ),
+            stockNumberColumn = manager.findStockNumberColumn()
+            thumbnailMod = new ItemThumbnailMod( stockNumberColumn ); 
+
+        manager.init();
+        // manager.install( thumbnailMod );
+        ready = true;
+    }
+
+    return { 
+        ItemsTableModManager: ItemsTableModManager,
+        ItemThumbnailMod: ItemThumbnailMod,
+        init: init
+    };
+})( jQuery );
